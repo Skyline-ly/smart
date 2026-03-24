cf788b43143d539eff17acb79d5831f5cb3653ca
Skyline-ly <eytaily123@gmail.com>
2026-03-24 15:13:16 +0700

    fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,72 @@
 package org.example
 
+import kotlin.reflect.typeOf
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
-    while (true) {
-        print("Enter your age: ")
-        val input = readLine()!!
-        val convert=input.toIntOrNull()
-
-        if (convert==null){
-            println("Invalid input")
-            continue
-        }
-        when{
-            convert>=18 -> {
-                println("adult")
-
-            }convert<18->{
-                println("Kid")
-            }
-        }
-        break;
+  println("Enter what u want")
+    var thing:String?=null
+    thing=readLine()
+    var count=thing?.length
+    println("the result is $count")
 
 
+//        var a: String? = "Hello"
+//        var b: String? = null
+//
+//        // --- Safe Call ?. ---
+//        println("Safe call (?.) results:")
+//        println(a?.length)  // 5, because a is not null
+//        println(b?.length)  // null, because b is null (safe, no crash)
+//
+//        // --- Non-null assertion !! ---
+//        println("\nNon-null assertion (!!) results:")
+//        println(a!!.length)  // 5, a is not null
+//         println(b!!.length)  // ❌ CRASH! b is null
 
-    }
+
+
+
+// prints type of name: String
+
+//println("enter what u want")
+//
+//    println("enter what u want")
+//    var name= ""
+//    name = readLine()?:""
+//    println(name?.uppercase())
+
+// prints "null" instead of crashing
+
+//        println("enter your age")
+//        val age = readLine()!!.toIntOrNull()
+//        val com=age?.%2==0
+//
+//        println("the com is $com")
+
+//    while (true) {
+//        print("Enter your age: ")
+//        val input = readLine()!!
+//        val convert=input.toIntOrNull()
+//
+//        if (convert==null){
+//            println("Invalid input")
+//            continue
+//        }
+//        when{
+//            convert>=18 -> {
+//                println("adult")
+//
+//            }convert<18->{
+//                println("Kid")
+//            }
+//        }
+//        break;
+//
+//
+//
+//    }
 
 }
